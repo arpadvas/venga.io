191370106ae2f0b283c5541d83a9688d857643d8
Arpad <arpad@yobetit.com>
2017-07-28 17:47:16 +0200

    add rest of the frontend valaidation

--- client/src/app/components/reg-form/reg-form.component.ts
+++ client/src/app/components/reg-form/reg-form.component.ts
@@ -18,11 +18,63 @@ export class RegFormComponent implements OnInit {
 
   createForm() {
     this.form = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(40),
+        this.validateName
+      ])],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(40),
+        this.validateEmail
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        this.validatePassword
+      ])],
       confirm: ['', Validators.required]
-    })
+    }, { validator: this.matchingPasswords('password', 'confirm') })
+  }
+
+  validateEmail(controls) {
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (regExp.test(controls.value)) {
+      return null; 
+    } else {
+      return { 'validateEmail': true } 
+    }
+  }
+
+  validateName(controls) {
+    const regExp = new RegExp(/^(([A-Za-z\u00C0-\u017F]{3,40})+[ ]+([A-Za-z\u00C0-\u017F]{3,40})+)+$/);
+    if (regExp.test(controls.value)) {
+      return null;
+    } else {
+      return { 'validateName': true }
+    }
+  }
+
+  validatePassword(controls) {
+    const regExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/);
+    if (regExp.test(controls.value)) {
+      return null;
+    } else {
+      return { 'validatePassword': true }
+    }
+  }
+
+  matchingPasswords(password, confirm) {
+    return (group: FormGroup) => {
+      if (group.controls[password].value === group.controls[confirm].value) {
+        return null;
+      } else {
+        return { 'matchingPasswords': true }
+      }
+    }
   }
 
   onRegisterSubmit() {
