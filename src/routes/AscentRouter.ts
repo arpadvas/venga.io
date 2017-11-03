788d7ba131c534c05a83e04b251e6a58138368e7
Arpad Vas <avas@mifinity.com>
2017-11-03 14:40:45 +0100

    modify get and post ascent routes

--- src/routes/AscentRouter.ts
+++ src/routes/AscentRouter.ts
@@ -3,6 +3,7 @@ import { User, IUserModel } from "../schemas/user";
 import { Ascent, IAscentModel } from "../schemas/ascent";
 import { asyncWrap } from "../helpers/async";
 import { config } from "../config/index";
+import requiresLogin from "../middlewares/requiresLogin";
 
 export class AscentRouter {
   router: Router;
@@ -28,22 +29,64 @@ export class AscentRouter {
   }
 
   /**
+   * Get actual user ascents.
+   */
+  public async getUserAscents(req: Request, res: Response, next: NextFunction): Promise<void> {
+    if ((<any>req)["decoded"]) {
+      const userId = (<any>req)["decoded"].userId;
+      const user = await User.findOne({ _id: userId });
+      if (user) {
+        const ascents = await Ascent.find({ senderId: userId });
+        if (!ascents || ascents.length === 0) {
+          return next(new Error("Could not find any ascents."));
+        } else {
+          res.send(ascents);
+        }
+      } else {
+        res.json({ success: false, message: "User is not found!" });
+      }
+    } else {
+      res.json({ success: false, message: "Token has not been provided!" });
+    }
+}
+
+  /**
    * Add new ascent.
    */
+
   public async addAscent(req: Request, res: Response, next: NextFunction): Promise<void> {
-    if (req.body.name && req.body.type && req.body.grade) {
-        const ascent = new Ascent({
-            name: req.body.name,
-            type: req.body.type,
-            grade: req.body.grade,
-            style: req.body.style,
-            sentDate: req.body.sentDate}); // get if from db
-        const ascentEntry = await ascent.save();
-        if (ascentEntry) {
-            res.json({ success: true, message: "Ascent has been saved.", ascent: ascent });
+    if ((<any>req)["decoded"]) {
+      const userId = (<any>req)["decoded"].userId;
+      const user = await User.findOne({ _id: userId });
+      if (user) {
+        if (req.body.name &&
+          req.body.type &&
+          req.body.grade &&
+          req.body.style &&
+          req.body.sentDate &&
+          req.body.cragId &&
+          req.body.sectorId) {
+          const ascent = new Ascent({
+              name: req.body.name,
+              type: req.body.type,
+              grade: req.body.grade,
+              style: req.body.style,
+              sentDate: req.body.sentDate,
+              senderId: userId,
+              cragId: req.body.cragId,
+              sectorId: req.body.sectorId});
+          const ascentEntry = await ascent.save();
+          if (ascentEntry) {
+              res.json({ success: true, message: "Ascent has been saved.", ascent: ascent });
+          }
+        } else {
+                res.json({ success: false, message: "Make sure ascent details were provided." });
         }
+      } else {
+        res.json({ success: false, message: "User is not found!" });
+      }
     } else {
-            res.json({ success: false, message: "Make sure ascent details were provided." });
+      res.json({ success: false, message: "Token has not been provided!" });
     }
   }
 
@@ -52,8 +95,9 @@ export class AscentRouter {
    * endpoints.
    */
   init(): void {
-    this.router.get("/ascents", asyncWrap(this.getAll));
-    this.router.post("/ascents", asyncWrap(this.addAscent));
+    this.router.get("/allascents", asyncWrap(this.getAll));
+    this.router.get("/ascents", requiresLogin, asyncWrap(this.getUserAscents));
+    this.router.post("/ascents", requiresLogin, asyncWrap(this.addAscent));
   }
 
 }
