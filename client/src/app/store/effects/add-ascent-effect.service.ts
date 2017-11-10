052d459a07de0a0385264a9b405f97a5aeeb079c
Arpad Vas <avas@Macs-MacBook-Pro.local>
2017-11-10 17:29:24 +0100

    minor update

--- client/src/app/store/effects/add-ascent-effect.service.ts
+++ client/src/app/store/effects/add-ascent-effect.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AscentsService } from '../../services/ascents.service';
 import { Actions, Effect } from '@ngrx/effects';
-import { ADD_NEW_ASCENT_ACTION, LoadAscentDataAction } from '../actions';
+import { ADD_NEW_ASCENT_ACTION, LoadAscentDataAction, AddNewAscentAction } from '../actions';
 import * as StateActions from  '../actions';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
@@ -16,9 +16,9 @@ export class AddAscentEffectService {
     private ascentsService: AscentsService
   ) { }
 
-  @Effect() newAscent$: Observable<any> = this.actions$
+  @Effect() newAscent$: Observable<Action> = this.actions$
     .ofType(ADD_NEW_ASCENT_ACTION)
-    .switchMap((action: any) => this.ascentsService.addAscent(action.payload))
+    .switchMap((action: AddNewAscentAction) => this.ascentsService.addAscent(action.payload))
     .map(action => new LoadAscentDataAction());
 
 }
