2cde60a2401d0d1a4e10bfa0b1f4b194afafe259
Arpad Vas <avas@Macs-MacBook-Pro.local>
2017-11-15 13:48:18 +0100

    add complete fieldbox to typeahead

--- client/src/app/shared/components/typeahead/typeahead.component.ts
+++ client/src/app/shared/components/typeahead/typeahead.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation, Input, ViewChild, ElementRef, for
 import { NG_VALUE_ACCESSOR, FormControl, ControlValueAccessor } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { AscentsService } from 'app/services/ascents.service';
+import * as _ from 'lodash';
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -19,22 +20,16 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
 export class TypeaheadComponent implements OnInit, ControlValueAccessor, AfterViewInit {
 
   @Input() type: string = 'text';
-  showDropdown: boolean = false;
   @Input() text: string = ''; 
   @Input() widthPercentage: string; 
   @Input() placeHolder: string;
   @Input() control: FormControl = new FormControl();
   @ViewChild('input') inputRef: ElementRef;
   private innerValue: string = '';
-  suggestions: any[];
+  private showDropdown: boolean = false;
+  suggestions: any[] = [];
 
-  constructor(private ascentsService: AscentsService) {
-  
-  }
-
-  fillTextbox(item) {
-
-  }
+  constructor(private ascentsService: AscentsService) { }
 
   get value(): string {
     return this.innerValue;
@@ -60,40 +55,54 @@ export class TypeaheadComponent implements OnInit, ControlValueAccessor, AfterVi
 
   onChange(e: Event, value: string){
     this.innerValue = value;
+    this.suggestions = [];
+    this.showDropdown = false;
     this.propagateChange(this.innerValue);
 }
 
-  ngAfterViewInit(){ 
+  ngAfterViewInit(){ }
 
+  query(value: string) {
+    return this.ascentsService.queryAscents(value);
+  }
+
+  fillTextBox(value: string) {
+    this.inputRef.nativeElement.value = value;
+    this.innerValue = value;
+    this.propagateChange(value);
+    this.showDropdown = false;
+  }
+
+  suggest() {
+    return this.control.valueChanges
+    .debounceTime(500)
+    .filter((fieldValue: string) => fieldValue.length > 0)
+    .filter((fieldValue: string) => _.map(this.suggestions, 'name').indexOf(fieldValue) === -1) // not calling API if suggestions name array contains field value 
+    .switchMap((fieldValue: string) => this.query(fieldValue))
+    .subscribe(result => {
+      if (result.payload) {
+        this.suggestions = result.payload;
+        this.showDropdown = true;
+      } else {
+        this.suggestions = [];
+        this.showDropdown = false;
+      }
+    });
   }
 
   ngOnInit() {
     if(this.placeHolder === undefined){
       this.placeHolder = 'Enter ' + this.text; 
     }
-    this.control.valueChanges.subscribe(
-        () => {
-            if (this.control.value == '' || this.control.value == null || this.control.value == undefined) {
-                this.innerValue = '';      
-                this.inputRef.nativeElement.value = '';
-                this.showDropdown = false;              
-            }
-            
-        }
+    this.control.valueChanges.subscribe(() => {
+        if (this.control.value == '' || this.control.value == null || this.control.value == undefined) {
+            this.innerValue = '';      
+            this.inputRef.nativeElement.value = '';
+            this.showDropdown = false;              
+        } 
+      }
     );
-    this.control.valueChanges
-      .debounceTime(500)
-      .filter((fieldValue: string) => fieldValue.length > 0)
-      .switchMap((fieldValue: string) => this.ascentsService.queryAscents(fieldValue))
-      .subscribe(result => {
-        if (result.payload) {
-          this.suggestions = result.payload;
-          this.showDropdown = true;
-        } else {
-          this.suggestions = [];
-          this.showDropdown = false;
-        }
-      });
+    this.suggest();
   }
 
 }
